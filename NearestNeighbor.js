93091b3f526834163deddca3483ddadbbd85d987
TobsterStrudel <tchubs15@gmail.com>
2022-04-21 05:44:03 -0400

    Completed implementation

--- NearestNeighbor.js
+++ NearestNeighbor.js
@@ -2,18 +2,16 @@ function closestPoint(current, points, visited){
     let min = Number.MAX_VALUE;
     let closest;
     for(let i = 0; i < points.length; i++) {
-        console.log(i);
         if (squaredDistance(current, points[i]) < min && squaredDistance(current, points[i]) !== 0 && !visited[i]) {
             min = squaredDistance(current, points[i]);
             closest = points[i];
-            console.log("8: " + points[i]);
         }
     }
     return closest;
 }
 function squaredDistance(a, b){
-    console.log(a**2+b**2);
-    return a**2 + b**2;
+    return (b[0] - a[0])**2 + (b[1] - a[1])**2
+
 }
 
 function nearestNeighbor(points){
@@ -21,20 +19,18 @@ function nearestNeighbor(points){
 
     for(let i = 0; i < points.length; i++){
         map.set(points[i], i);
-        console.log(points[i]);
     }
-    let pathIds = [];
+    var pathIds = [];
     let visited = new Array(points.length).fill(false);
-
+    pathIds.push(1)
     let i = 0;
     while(pathIds.length < points.length){ // O(n)
         visited[i] = true;
-        console.log("visited[i]: " + visited[i]);
-        console.log("points[i]: " + points[i]);
-        console.log("i: " + i);
         let closest = closestPoint(points[i], points, visited); // O(n)
-        pathIds.push(map.get(closest));
+        pathIds.push(map.get(closest)+1);
         i = map.get(closest);
     }
-    console.log(pathIds);
+    pathIds.push(1);
+    console.log("pathIds: " + pathIds);
+    document.getElementById("path").innerHTML = "Path: " + pathIds
 }
