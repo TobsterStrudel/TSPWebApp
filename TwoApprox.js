464e16c8f84647d667a8570d546df5b0f09f0c2e
TobsterStrudel <tchubs15@gmail.com>
2022-04-21 07:47:11 -0400

    Half working prims algorithm. Fixes need, correct around 80%

--- TwoApprox.js
+++ TwoApprox.js
@@ -1,6 +1,7 @@
 function twoApprox(points){
     const map = new Map();
     const graph = new Map();
+    var pathIds = [];
     for(let i = 0; i < points.length; i++){
         map.set(points[i], i);
     }
@@ -9,10 +10,44 @@ function twoApprox(points){
         for(let j = 0; j < points.length; j++){
             if(i !== j){
                 temp.push([j, squaredDistance(points[i], points[j]).toFixed(0)]);
+            }else{
+                temp.push([j,0]);
             }
         }
         graph.set(i, temp);
         console.log(graph.get(i));
     }
+    let x, y;
+    let edges = 0;
+    let selected = new Array(points.length).fill(false);
+    let current = graph.get(0);
+    selected[0] = true;
+    let mst = [];
+    while(edges < points.length - 1){
+        let min = Number.MAX_VALUE; //or I guess it could be 500000 since the window is 500 x 500
+        x = 0;
+        y = 0;
+
+        for(let i = 0; i < points.length; i++){
+            if(selected[i]){
+                for(let j = 0; j < points.length; j++){
+                    if(!selected[j] && graph.get(i)[j][1] !== 0){
+                        if(min > graph.get(i)[j][1]){
+                            min = graph.get(i)[j][1];
+                            console.log("min: " + graph.get(i)[j]);
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+
+        }
+        console.log(x + " - " + y + " - " + graph.get(x)[y][1]);
+
+        selected[y] = true;
+        edges++;
+    }
+
 
 }
