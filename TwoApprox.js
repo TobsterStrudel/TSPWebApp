91b1f76a10a325117fc64071c2cf17697f616bed
TobsterStrudel <tchubs15@gmail.com>
2022-04-21 16:05:38 -0400

    Created TreeNode class for binary tree

--- TwoApprox.js
+++ TwoApprox.js
@@ -1,3 +1,9 @@
+class TreeNode{
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+}
 function twoApprox(points){
     const map = new Map();
     const graph = new Map();
@@ -23,7 +29,6 @@ function twoApprox(points){
     let current = graph.get(0);
     selected[0] = true;
     let mst = [];
-    mst.push(0);
     while(edges < points.length - 1){
         let min = Number.MAX_VALUE; //or I guess it could be 500000 since the window is 500 x 500
         x = 0;
@@ -44,10 +49,12 @@ function twoApprox(points){
             }
         }
         console.log(x + " - " + y + " - " + graph.get(x)[y][1]);
-        mst.push(graph.get(x)[y][0]);
+        mst.push([x,y]);
         selected[y] = true;
         edges++;
     }
     console.log(mst);
+
+    const root = new TreeNode(mst[0][0]);
 
 }
