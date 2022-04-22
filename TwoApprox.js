d56be5793217027e6212a1227103724781ce315c
TobsterStrudel <tchubs15@gmail.com>
2022-04-22 07:37:41 -0400

    Reformatted, added comments

--- TwoApprox.js
+++ TwoApprox.js
@@ -1,26 +1,27 @@
-var adj;
-var path = [];
+let adj;
+const path = [];
 
 function twoApprox(points){
     const idsToPointsMap = new Map();
     for(let i = 0; i < points.length; i++){
         idsToPointsMap.set(i, points[i]);
     }
-    path = [];
+
     const graph = createGraph(points);
     let mst = prims(graph);
 
-    adj = Array.from(Array(points.length).fill(0), () => new Array(points.length).fill(0));
+    adj = Array.from(Array(points.length).fill(0), () => new Array(points.length).fill(0)); //nxn array filled with zeros
 
-    for(let i = 0; i < mst.length; i++){
-        addEdge(mst[i][0], mst[i][1])
+    for(let i = 0; i < mst.length; i++){ //building adjacency matrix inside adj
+        addEdge(mst[i][0], mst[i][1]) // 0 & 1 represent parent and child respectively
     }
     let visited = new Array(points.length).fill(false);
-    dfs(0,visited);
+    dfs(0,visited); //result is stored in path (global variable)
     path.push(1);
-    d3.selectAll("line").remove();
+
+    d3.selectAll("line").remove(); //remove any lines made by other functions before printing more
     document.getElementById("path").innerHTML = "Path: " + path;
-    for(let i = 0; i < path.length-1; i++){
+    for(let i = 0; i < path.length-1; i++){ //send arrows to UI
         makeArrow(idsToPointsMap.get(path[i]-1), idsToPointsMap.get(path[i+1]-1));
     }
 }
