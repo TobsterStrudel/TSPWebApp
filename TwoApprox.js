6ccb088be39a51119d2e6cf73e7eb4eeec1cc0ba
TobsterStrudel <tchubs15@gmail.com>
2022-04-22 04:20:58 -0400

    Seperated MST calculation to standalone function prims() Changed 1st index from 0 to 1 for ease of reading.

--- TwoApprox.js
+++ TwoApprox.js
@@ -1,5 +1,6 @@
 var adj;
 var path = [];
+
 function twoApprox(points){
     path = [];
     const map = new Map();
@@ -18,6 +19,33 @@ function twoApprox(points){
         }
         graph.set(i, temp);
     }
+    let mst = prims(graph);
+
+    adj = Array.from(Array(points.length).fill(0), () => new Array(points.length).fill(0));
+
+    for(let i = 0; i < mst.length; i++){
+        addEdge(mst[i][0], mst[i][1])
+    }
+    let visited = new Array(points.length).fill(false);
+    dfs(0,visited);
+    path.push(1);
+    document.getElementById("path").innerHTML = "Path: " + path;
+}
+function dfs(start, visited){
+    path.push(start+1);
+    visited[start] = true;
+
+    for(let i = 0; i < adj[start].length; i++){
+        if(adj[start][i] === 1 && !visited[i])
+            dfs(i, visited);
+    }
+}
+function addEdge(x, y)
+{
+    adj[x][y] = 1;
+    adj[y][x] = 1;
+}
+function prims(graph){
     let x, y;
     let edges = 0;
     let selected = new Array(points.length).fill(false);
@@ -27,7 +55,6 @@ function twoApprox(points){
         let min = Number.MAX_VALUE; //or I guess it could be 500000 since the window is 500 x 500
         x = 0;
         y = 0;
-
         for(let i = 0; i < points.length; i++){
             if(selected[i]){
                 for(let j = 0; j < points.length; j++){
@@ -46,28 +73,5 @@ function twoApprox(points){
         selected[y] = true;
         edges++;
     }
-
-    adj = Array.from(Array(points.length).fill(0), () => new Array(points.length).fill(0));
-
-    for(let i = 0; i < mst.length; i++){
-        addEdge(mst[i][0], mst[i][1])
-    }
-    let visited = new Array(points.length).fill(false);
-    dfs(0,visited);
-    path.push(0);
-    document.getElementById("path").innerHTML = "Path: " + path;
-}
-function dfs(start, visited){
-    path.push(start);
-    visited[start] = true;
-
-    for(let i = 0; i < adj[start].length; i++){
-        if(adj[start][i] === 1 && !visited[i])
-            dfs(i, visited);
-    }
-}
-function addEdge(x, y)
-{
-    adj[x][y] = 1;
-    adj[y][x] = 1;
+    return mst;
 }
