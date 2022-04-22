dca4f04e668664ba468843398c1c107ce8e3d2bf
TobsterStrudel <tchubs15@gmail.com>
2022-04-22 06:20:59 -0400

    Added oddGraph

--- ChristofidesApprox.js
+++ ChristofidesApprox.js
@@ -1,8 +1,16 @@
 function christofides(points){
-    let mst = prims(createGraph(points));
+    const map = new Map();
+
+    for(let i = 0; i < points.length; i++){
+        map.set(i, points[i]);
+    }
+    const graph = createGraph(points)
+    console.log(graph);
+    let mst = prims(graph);
     console.log(mst);
     let str = "";
     let odd = [];
+    let oddPoints = [];
     for(let i = 0; i < mst.length; i++){
         str += mst[i][0];
         str += mst[i][1];
@@ -10,8 +18,11 @@ function christofides(points){
     for(let i = 0; i < points.length; i++){
         if((str.split(i.toString()).length - 1) % 2 === 1){
             odd.push(i);
+            oddPoints.push(map.get(i));
         }
     }
     console.log(odd);
+    const oddGraph = createGraph(oddPoints)
+    console.log(oddGraph);
 
 }
