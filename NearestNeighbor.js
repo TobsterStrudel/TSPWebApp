637497e256a2f5b9d8f42984c44c573259d91d93
TobsterStrudel <tchubs15@gmail.com>
2022-04-22 07:29:05 -0400

    Moved all helper functions to algorithms.js for organization

--- NearestNeighbor.js
+++ NearestNeighbor.js
@@ -1,18 +1,3 @@
-function closestPoint(current, points, visited){
-    let min = Number.MAX_VALUE;
-    let closest;
-    for(let i = 0; i < points.length; i++) {
-        if (squaredDistance(current, points[i]) < min && squaredDistance(current, points[i]) !== 0 && !visited[i]) {
-            min = squaredDistance(current, points[i]);
-            closest = points[i];
-        }
-    }
-    return closest;
-}
-function squaredDistance(a, b){
-    return (b[0] - a[0])**2 + (b[1] - a[1])**2
-
-}
 
 function nearestNeighbor(points){
     const pointstoIdsMap = new Map();
@@ -24,7 +9,8 @@ function nearestNeighbor(points){
     for(let i = 0; i < points.length; i++){
         idsToPointsMap.set(i, points[i]);
     }
-    var pathIds = [];
+
+    const pathIds = [];
     let visited = new Array(points.length).fill(false);
     pathIds.push(1)
     let i = 0;
@@ -35,19 +21,10 @@ function nearestNeighbor(points){
         i = pointstoIdsMap.get(closest);
     }
     pathIds.push(1);
+
     d3.selectAll("line").remove();
     document.getElementById("path").innerHTML = "Path: " + pathIds
     for(let i = 0; i < pathIds.length-1; i++){
         makeArrow(idsToPointsMap.get(pathIds[i]-1), idsToPointsMap.get(pathIds[i+1]-1));
     }
-}
-function makeArrow(a, b){
-    svg.append("line")
-        .attr("x1",a[0])
-        .attr("y1",a[1])
-        .attr("x2",b[0])
-        .attr("y2",b[1])
-        .attr("stroke","blue")
-        .attr("stroke-width",2)
-        .attr("marker-end","url(#arrow)");
 }
---
+++ algorithms.js
@@ -0,0 +1,73 @@
+function addEdge(x, y)
+{
+    adj[x][y] = 1;
+    adj[y][x] = 1;
+}
+function dfs(start, visited){
+    path.push(start+1);
+    visited[start] = true;
+
+    for(let i = 0; i < adj[start].length; i++){
+        if(adj[start][i] === 1 && !visited[i])
+            dfs(i, visited);
+    }
+}
+function prims(graph){
+    let x, y;
+    let edges = 0;
+    let selected = new Array(points.length).fill(false);
+    selected[0] = true;
+    let mst = [];
+    while(edges < points.length - 1){
+        let min = Number.MAX_VALUE; //or I guess it could be 500000 since the window is 500 x 500
+        x = 0;
+        y = 0;
+        for(let i = 0; i < points.length; i++){
+            if(selected[i]){
+                for(let j = 0; j < points.length; j++){
+                    let current = parseInt(graph.get(i)[j][1]);
+                    if(!selected[j] && current !== 0){
+                        if(current < min){
+                            min = current;
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+        }
+        mst.push([x,y]);
+        selected[y] = true;
+        edges++;
+    }
+    return mst;
+}
+function createGraph(points){
+    const graph = new Map();
+    for(let i = 0; i < points.length; i++){
+        let temp = [];
+        for(let j = 0; j < points.length; j++){
+            if(i !== j){
+                temp.push([j, squaredDistance(points[i], points[j]).toFixed(0)]);
+            }else{
+                temp.push([j,0]);
+            }
+        }
+        graph.set(i, temp);
+    }
+    return graph;
+}
+function squaredDistance(a, b){
+    return (b[0] - a[0])**2 + (b[1] - a[1])**2
+}
+function closestPoint(current, points, visited){
+    let min = Number.MAX_VALUE;
+    let closest;
+    for(let i = 0; i < points.length; i++) {
+        if (squaredDistance(current, points[i]) < min && squaredDistance(current, points[i]) !== 0 && !visited[i]) {
+            min = squaredDistance(current, points[i]);
+            closest = points[i];
+        }
+    }
+    return closest;
+}
